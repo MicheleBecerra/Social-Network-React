2d4d8c24d1d1e5700fb9aa123aa39380ba2c69fb
michele velazquez de la rosa becerra <michbecerra@hotmail.com>
2018-10-17 10:34:15 -0500

    Agregando boton de cierre en el inicio de la red social

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import express from 'express'
+import morgan from 'morgan'
+import path from 'path'
+
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
@@ -11,6 +15,25 @@ import createHistory from 'history/createBrowserHistory'
 import { routerMiddleware } from 'react-router-redux'
 import thunk from 'redux-thunk'
 import reducers from './reducers'
+
+const app = express()
+
+// Settings: se establece la configuracion del puerto, ya que puede tomar el puerto que nos indiquen al hacer el deployd o tomar el puerto local
+app.set('port', process.env.PORT || 3000)
+// Los Middelware: son funciones que se ejecutan cada vez que hay una peticion al servidor. Pero se ejecutan antes de que lleguen las rutas.
+app.use(morgan('dev'))
+app.use(express.json()) // Para entender un formato de intercambio de datos, va a funcionar cada vez que llegue una peticion al servidor, se tiene que asegurar que esta solicitud en formato .json
+
+// Routes: se define  un rest API, para manejar las peticiones y obtener datos, actuualizarlo y eliminarlos
+app.use('/api/tasks', require('./routes/task-routes'))
+
+// Static files
+app.use(express.static(path.join(__dirname, '/public')))
+// console.log(path.join(__dirname, '/public'))
+
+app.listen(app.get('port'), () => {
+  console.log(`server on port ${app.get('port')}`)
+})
 
 const history = createHistory()
 // se conecta el router con redux con  dos middelware history y thunk
